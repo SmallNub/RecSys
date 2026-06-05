a7b7945acbd71a288aca916bc4281ece1b912bff
SakrIsmail <sakrismail265@gmail.com>
2026-06-05 12:24:45 +0200

    run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=cosette_training
-#SBATCH --output=slurm/cosette_training_%j.log
-#SBATCH --error=slurm/cosette_training_%j.err
+#SBATCH --output=scripts/slurm/cosette_training_%j.log
+#SBATCH --error=scripts/slurm/cosette_training_%j.err
 #SBATCH --time=24:00:00
 #SBATCH --partition=gpu_h100
 #SBATCH --nodes=1
@@ -17,8 +17,6 @@ module load Anaconda3/2025.06-1
 module load CUDA/12.4.0
 
 source activate recsys
-
-cd ..
 
 python data_scripts/0_raw_to_parquet.py
 python data_scripts/1_make_embeddings.py
